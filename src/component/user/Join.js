ff252838cc083b34f346f10ef09aa2006d17757a
JOY0987 <jjoyewon@gmail.com>
2023-06-09 09:49:55 +0900

    [React] 회원가입 입력

--- src/component/user/Join.js
+++ src/component/user/Join.js
@@ -1,6 +1,15 @@
-import React, { useEffect, useState } from 'react'
-import {Button, Container, Grid,
-  TextField, Typography, Link} from "@mui/material";
+import React, {
+  useEffect,
+  useState
+} from 'react';
+import {
+  Button,
+  Container,
+  Grid,
+  TextField,
+  Typography,
+  Link
+} from "@mui/material";
 
 const Join = () => {
 
@@ -13,95 +22,168 @@ const Join = () => {
 
   // 검증 메세지에 대한 상태변수 관리
   const [message, setMessage] = useState({
-    // 검증할 게 4개니까 객체로 관리합니다~~
     userName: '',
     password: '',
     passwordCheck: '',
     email: ''
   });
 
+  // 검증 완료 체크에 대한 상태변수 관리
+  const [correct, setCorrect] = useState({
+    userName: false,
+    password: false,
+    passwordCheck: false,
+    email: false
+  });
+
+
+  // 검증데이터를 상태변수에 저장하는 함수
+  const saveInputState = ({
+    key,
+    inputVal,
+    flag,
+    msg
+  }) => {
+
+    inputVal !== 'pass' && setUserValue({
+      ...userValue,
+      [key]: inputVal
+    });
+
+    setMessage({
+      ...message,
+      [key]: msg
+    });
+
+
+    setCorrect({
+      ...correct,
+      [key]: flag
+    });
+  };
+
+
   // 이름 입력창 체인지 이벤트 핸들러
   const nameHandler = e => {
 
-    // 입력값 검증
     const nameRegex = /^[가-힣]{2,5}$/;
-    const inputVal = e.target.value;
 
-    let msg; // 검증 메세지를 저장할 변수 (ex. ~가 틀렸습니다!)
+    const inputVal = e.target.value;
+    // 입력값 검증
+    let msg; // 검증 메시지를 저장할 변수
+    let flag; // 입력 검증체크 변수
 
     if (!inputVal) {
       msg = '유저 이름은 필수입니다.';
+      flag = false;
     } else if (!nameRegex.test(inputVal)) {
       msg = '2~5글자 사이의 한글로 작성하세요!';
+      flag = false;
     } else {
       msg = '사용 가능한 이름입니다.';
+      flag = true;
     }
 
-    // 검증 메세지 세팅
-    setMessage({
-      ...message, // 기존 메세지 가져오기
-      userName: msg // userName의 메세지만 변경
+    saveInputState({
+      key: 'userName',
+      inputVal,
+      msg,
+      flag
     });
 
-    // 입력한 값을 상태변수에 저장
-    console.log(e.target.value);
-
-
-    setUserValue({
-      ...userValue, // 기존값 가져오기
-      userName: inputVal // userName 만 변경
-    });
 
   };
 
   // 이메일 입력창 체인지 이벤트 핸들러
   const emailHandler = e => {
 
-    // 입력한 값을 상태변수에 저장
-    console.log(e.target.value);
-
     const inputVal = e.target.value;
 
     setUserValue({
-      ...userValue, // 기존값 가져오기
-      email: inputVal // userName 만 변경
+      ...userValue,
+      email: inputVal
     });
 
   };
 
-  // 비밀번호 입력창 체인지 이벤트 핸들러
+  // 패스워드 입력창 체인지 이벤트 핸들러
   const passwordHandler = e => {
 
-    // 입력한 값을 상태변수에 저장
-    console.log(e.target.value);
+    // 패스워드가 변동되면 확인란을 비우기
+    document.getElementById('password-check').value = '';
+    document.getElementById('check-span').textContent = '';
+
+    setMessage({...message, passwordCheck: ''}); // 화면을 다시 랜더링해야 메세지가 바뀐다,,,
+    setCorrect({...correct, passwordCheck: false});
 
     const inputVal = e.target.value;
 
-    setUserValue({
-      ...userValue, // 기존값 가져오기
-      password: inputVal // userName 만 변경
+    const pwRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,20}$/;
+
+    // 검증 시작
+    let msg, flag;
+    if (!e.target.value) { // 패스워드 안적은거
+      msg = '비밀번호는 필수값입니다!';
+      flag = false;
+    } else if (!pwRegex.test(e.target.value)) {
+      msg = '8글자 이상의 영문,숫자,특수문자를 포함해주세요!';
+      flag = false;
+    } else {
+      msg = '사용 가능한 비밀번호입니다.';
+      flag = true;
+    }
+
+    saveInputState({
+      key: 'password',
+      inputVal,
+      msg,
+      flag
     });
 
   };
+
+  // 비밀번호 확인란 검증 이벤트 핸들러
+  // 패스워드 란에서 change 이벤트가 발생하면 확인란의 입력값을 지워버려야한다.
+  const pwCheckHandler = e => {
+    // 검증 시작
+    let msg, flag;
+    if (!e.target.value) { // 패스워드 안적은거
+      msg = '비밀번호 확인란은 필수값입니다!';
+      flag = false;
+    } else if (userValue.password !== e.target.value) {
+      msg = '패스워드가 일치하지 않습니다.';
+      flag = false;
+    } else {
+      msg = '패스워드가 일치합니다.';
+      flag = true;
+    }
+
+    saveInputState({
+      key: 'passwordCheck',
+      inputVal: 'pass',
+      msg,
+      flag
+    });
+
+  };
+
+
 
   const joinButtonClickHandler = e => {
 
     e.preventDefault();
 
-    // console.log($nameInput.value);
-    // 상태의 불변성때문에 객체를 통째로 갈아끼워줘야 한다.
-    // 반드시 세터를 통해서 상태를 변경해줘야 한다.
     console.log(userValue);
+  };
 
-  }
-  
+
   // 렌더링이 끝난 이후 실행되는 함수
   useEffect(() => {
-    
+
   }, []);
 
   return (
-    <Container component="main" maxWidth="xs" style={{ margin: "300px auto" }}>
+    <Container component="main" maxWidth="xs" style={{ margin: "200px auto" }}>
         <form noValidate>
             <Grid container spacing={2}>
                 <Grid item xs={12}>
@@ -121,7 +203,12 @@ const Join = () => {
                         autoFocus
                         onChange={nameHandler}
                     />
-                    <span>{message.userName}</span>
+                    <span style={
+                      correct.userName
+                      ?{color:'green'}
+                      : {color:'red'}
+                    }>{message.userName}</span>
+                    {/* 문자열만 {}생략가능 다른 모든것은 {}써야한다. */}
                 </Grid>
                 <Grid item xs={12}>
                     <TextField
@@ -148,7 +235,11 @@ const Join = () => {
                         autoComplete="current-password"
                         onChange={passwordHandler}
                     />
-                    <span></span>
+                    <span style={
+                      correct.password
+                      ?{color:'green'}
+                      : {color:'red'}
+                    }>{message.password}</span>
                 </Grid>
 
                 <Grid item xs={12}>
@@ -161,17 +252,23 @@ const Join = () => {
                         type="password"
                         id="password-check"
                         autoComplete="check-password"
-                
+                        onChange={pwCheckHandler}                
                     />
-                    <span id="check-text"></span>
+                    <span id='check-span' style={
+                      correct.passwordCheck
+                      ? {color:'green'}
+                      : {color:'red'}
+                    }>{message.passwordCheck}</span>
                 </Grid>
 
                 <Grid item xs={12}>
                     <Button 
                       type="submit" 
-                      fullWidth variant="contained" 
+                      fullWidth 
+                      variant="contained" 
                       style={{background: '#38d9a9'}}
-                      onClick={joinButtonClickHandler}>
+                      onClick={joinButtonClickHandler}
+                    >
                         계정 생성
                     </Button>
                 </Grid>
@@ -186,6 +283,7 @@ const Join = () => {
         </form>
     </Container>
   );
+
 }
 
-export default Join;
+export default Join
